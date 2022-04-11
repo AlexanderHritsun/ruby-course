8026f9112c76ce8f887dbe5ee764539cc8739c80
Alexander Hritsun <47444468+AlexanderHritsun@users.noreply.github.com>
2022-04-11 17:43:44 +0300

    Update ranges.rb

--- data_types/ranges.rb
+++ data_types/ranges.rb
@@ -8,3 +8,20 @@ puts
 (10...15).each do |n|
   print n, ' '
 end
+
+current_date = DateTime.new
+end_day_of_the_previous_month = current_date.prev_month(1).end_of_month.day
+first_date_of_the_previous_month = current_date.prev_month(1).beginning_of_month
+
+(1..end_day_of_the_previous_month).each { |day|
+  if day == 3
+    create(:time_entry, minutes: 270, spent_date: first_date_of_the_previous_month)
+  elsif day == 10
+    create(:time_entry, minutes: 124, spent_date: first_date_of_the_previous_month)
+  elsif day == 16
+    create(:time_entry, minutes: 287, spent_date: first_date_of_the_previous_month)
+  else
+    create(:time_entry, spent_date: first_date_of_the_previous_month)
+
+  first_date_of_the_previous_month + 1.day
+}
